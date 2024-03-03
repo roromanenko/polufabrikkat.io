218fa41a029babb8ded3feaaede2a4c0a29823dc
Roman Romanenko <roromanenko2004@gmail.com>
2024-03-03 17:16:06 +0200

    programming space

---
+++ Polufabrikkat/Polufabrikkat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Polufabrikkat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
