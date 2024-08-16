9dda09fc3d575b6c45808bf175b89575bfdedd1b
Yevhenii.Romanenko <zerom2016romanenko@gmail.com>
2024-08-16 12:39:30 +0300

    temp tiktok login

--- Polufabrikkat/Polufabrikkat/Controllers/HomeController.cs
+++ Polufabrikkat/Polufabrikkat/Controllers/HomeController.cs
@@ -4,17 +4,24 @@ using Polufabrikkat.Models;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Security.AccessControl;
+using System.Text;
+using System.Text.Json;
+using System.Web;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace Polufabrikkat.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+		private readonly HttpClient _httpClient;
 
-        public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, HttpClient httpClient)
         {
             _logger = logger;
-        }
+			_httpClient = httpClient;
+		}
 
         public async Task<IActionResult> Index()
         {
@@ -40,7 +47,7 @@ namespace Polufabrikkat.Controllers
 		public IActionResult RedirectToTikTokLogin()
 		{
 			var clientKey = "***REMOVED***"; // from tiktok dev
-			var scope = "user.info.basic";
+			var scope = "user.info.basic,video.publish,video.upload";
 			var redirectUrl = Url.Action("ProcessTikTokLoginResponse", "Home", null, Request.Scheme, Request.Host.Value);
 			var uniqueIdentificatorState = Guid.NewGuid().ToString("N");
 			var responseType = "code";
@@ -61,11 +68,45 @@ namespace Polufabrikkat.Controllers
 			return Redirect(url.ToString());
 		}
 
-		public IActionResult ProcessTikTokLoginResponse()
+		public async Task<IActionResult> ProcessTikTokLoginResponse()
 		{
-			var response = Response;
+			if (!Request.Query.ContainsKey("access_token"))
+			{
+				var response = new
+				{
+					Code = Request.Query["code"],
+					Scopes = Request.Query["scopes"],
+					State = Request.Query["state"],
+					Error = Request.Query["error"],
+					Error_description = Request.Query["error_description"],
+				};
+
+				var clientKey = "***REMOVED***"; // from tiktok dev
+				var clientSecret = "***REMOVED***";
+				var redirectUrl = Url.Action("ProcessTikTokLoginResponse", "Home", null, Request.Scheme, Request.Host.Value);
+
+				var accessTokenUrl = "https://open.tiktokapis.com/v2/oauth/token/";
+				var queryString = new Dictionary<string, string>()
+				{
+					["client_key"] = clientKey,
+					["client_secret"] = clientSecret,
+					["code"] = HttpUtility.UrlDecode(response.Code),
+					["grant_type"] = "authorization_code",
+					["redirect_uri"] = redirectUrl,
+				};
+
+				using var request = new HttpRequestMessage(HttpMethod.Post, accessTokenUrl)
+				{
+					Content = new FormUrlEncodedContent(queryString),
+				};
+				//request.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+				using var res = await _httpClient.SendAsync(request);
+
+				var content = await res.Content.ReadAsStringAsync();
+
+			}
+			var acctresponse = Request;
 			return RedirectToAction("Index", "Home");
 		}
-
 	}
 }
--- Polufabrikkat/Polufabrikkat/Program.cs
+++ Polufabrikkat/Polufabrikkat/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddCors(options =>
                       });
 });
 
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
